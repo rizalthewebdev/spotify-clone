ca94bf5152d64e1283d98821408ab6fbc5e9d254
rzlTrimelty <rzl.trimelty@gmail.com>
2022-02-05 07:08:33 +0700

    Setting

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+   content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {},
+      colors: {
+        'darkBg' : '#121212'
+      },
+   },
+   plugins: [],
+};
